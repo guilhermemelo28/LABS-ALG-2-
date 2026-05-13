016a17c4721e9b5c8a80955859f65a602430aad2
guilhermemelo28 <guimelomoura28@gmail.com>
2026-05-13 15:00:41 -0300

    atualizacao-e-delet

--- aula1.c
+++ aula1.c
@@ -5,126 +5,126 @@
 int main ()
 
 {
-//ex 1
-int fumaca, temperatura;
+        //ex 1
+        int fumaca, temperatura;
 
-//ex 2
-int numero, resto, verifica;
+        //ex 2
+        int numero, resto, verifica;
 
-//ex3
-char vet[101], max = 1, atual = 1;
+        //ex3
+        char vet[101], max = 1, atual = 1;
 
-int i,q = 3;
-
-
-for ( i = 0; i < (q + 1); i++){
+        int i,q = 3;
 
 
-switch ( i + 1)
-{
-case  1:
-    //questao de if else
-    printf("Questao %d: \n", i + 1);
+        for ( i = 0; i < (q + 1); i++){
 
-    printf("Digite o nivel de fumaca: ");
-    scanf("%d",&fumaca);
-    printf("\n");
 
-    printf("Digite a temperatura em graus celcius: ");
-    scanf("%d",&temperatura);
-    printf("\n");
+        switch ( i + 1)
+        {
+        case  1:
+            //questao de if else
+            printf("Questao %d: \n", i + 1);
 
-    if((temperatura < 40) && (fumaca < 20))
-    {
+            printf("Digite o nivel de fumaca: ");
+            scanf("%d",&fumaca);
+            printf("\n");
 
-        printf("Seguro!\n");
+            printf("Digite a temperatura em graus celcius: ");
+            scanf("%d",&temperatura);
+            printf("\n");
 
-    }
+            if((temperatura < 40) && (fumaca < 20))
+            {
 
-    else if (((temperatura <= 60) && (temperatura >= 40)) || ((fumaca >= 20) && (fumaca <= 50) ))
-    {
+                printf("Seguro!\n");
 
-        printf(" Alerta!\n");
-         
-    } 
-    else if ((temperatura > 50) && (fumaca > 40))
-    {
+            }
 
-        printf("Critico!\n");
-         
-    }
+            else if (((temperatura <= 60) && (temperatura >= 40)) || ((fumaca >= 20) && (fumaca <= 50) ))
+            {
 
-    else 
-    {
+                printf(" Alerta!\n");
+                
+            } 
+            else if ((temperatura > 50) && (fumaca > 40))
+            {
 
-        printf("Perigo!\n");
-        
-    }
+                printf("Critico!\n");
+            
+            }
 
-    
+            else 
+            {
 
-    break;
-    case 2:
+                printf("Perigo!\n");
+                
+            }
 
-    //loop de repeticao
-     printf("Questao %d: \n", i + 1);
-    printf("Numero:\n");
-    scanf("%d",&numero);
+            
 
-     while (numero > 0){
-    
-        resto = numero % 10;
+            break;
+            case 2:
 
-        if ((numero % resto) == 0){
-            numero /= 10;
-            verifica = 1;
-        }
-        else{
-            verifica = 0;
+            //loop de repeticao
+            printf("Questao %d: \n", i + 1);
+            printf("Numero:\n");
+            scanf("%d",&numero);
+
+            while (numero > 0){
+            
+                resto = numero % 10;
+
+                if ((numero % resto) == 0){
+                    numero /= 10;
+                    verifica = 1;
+                }
+                else{
+                    verifica = 0;
+                    break;
+                }
+                numero /= 10;
+
+            }
+
+            printf((verifica == 1) ? "SIM\n" : "NAO\n");
+
+
+            break;
+
+
+
+            case 3:
+            //string e vetor
+            printf("Questao %d: \n", i + 1);
+
+                printf("Digite um string:\n");
+                scanf("%s",vet);
+
+                    for (i = 1; vet[i] != '\0'; i++){
+
+                    if ( vet[i] == vet[i-1]){
+                        atual++;
+                    }else {
+                        max = (atual > max) ? atual : max;
+                        atual = 1;
+                    }
+                }
+
+                    max = (atual > max) ? atual : max;
+                    printf("%d\n",max);
+
+
+            break;
+
+            default:
+            printf("Acabou as questoes :( ");
+
             break;
         }
-        numero /= 10;
-
-     }
-
-     printf((verifica == 1) ? "SIM\n" : "NAO\n");
 
 
-    break;
-
-
-
-    case 3:
-    //string e vetor
-     printf("Questao %d: \n", i + 1);
-
-        printf("Digite um string:\n");
-        scanf("%s",vet);
-
-            for (i = 1; vet[i] != '\0'; i++){
-
-            if ( vet[i] == vet[i-1]){
-                atual++;
-            }else {
-                max = (atual > max) ? atual : max;
-                atual = 1;
-            }
         }
-
-            max = (atual > max) ? atual : max;
-            printf("%d\n",max);
-
-
-    break;
-
-    default:
-    printf("Acabou as questoes :( ");
-
-    break;
-}
-
-
-}
 
 return 0;
 }
---
+++ ex1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+
+void ano (int i){
+    char mes[12][8];
+}
