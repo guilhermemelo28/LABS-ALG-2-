5038bedee9720cdb045ed16aedf310e001217834
guilhermemelo28 <guimelomoura28@gmail.com>
2026-06-15 16:26:32 -0300

    Ex2 feito graças a Deus

--- modularização/lista2/ex2/main.c
+++ modularização/lista2/ex2/main.c
@@ -3,14 +3,85 @@
 
 int main()
 {
-    char s[10];
-    int t ;
-    printf("Digite\n");
-    scanf("%s",s);
+  char b[50];
+  char s[50];
+  char copia1[50];
+  char copia2[50];
+  
+  int p, t, respMstrcmp;
+  printf("Digite\n");
+  fgets(s, 50, stdin);
 
-    t = mstrlen(s);
+  printf("Digite\n");
+  fgets(b, 50, stdin);
 
-    printf("%d",t);
+  // tamanho string
+  t = mstrlen(s);
+  printf("Tamanho da primeira string digitada:%d\n", t);
+  p = mstrlen(b);
+  printf("Tamanho da segunda string digitada:%d\n", p);
 
-    return 0;
+  printf("Copia da primeira string:\n");
+  // copia string
+  mstrcpy(copia1, s);
+
+  printf("Copia da segunda string:\n");
+  mstrcpy(copia2, b);
+
+  // concatenação
+  mstrcat(s, b);
+  printf("Concatenacao: %s\n", s);
+
+  // mstrcmp
+  respMstrcmp = mstrcmp(b,s );
+  printf("0 - Se as strings forem iguais.\n1 - se a primeira string vier antes que a segunda string.\nE -1 : se a segunda string vier antes que a primeira string\n");
+  printf("Resposta: %d\n", respMstrcmp);
+
+  // mstrupper
+  mstrupper(copia1);
+  printf("Primeira string maiuscula: %s\n", copia1);
+
+  mstrupper(copia2);
+  printf("Segunda string maiuscula: %s\n", copia2);
+
+  // mstrlower
+  mstrlower(copia1);
+  printf("Primeira string minuscula : %s\n", copia1);
+  mstrlower(copia2);
+  printf("Segunda string minuscula: %s\n", copia2);
+
+  // qtd vogais
+  printf("Quantidade de vogais da primeira string :%d\n", mcount_vogais(copia1));
+  printf("Quantidade de vogais da segunda string: %d\n", mcount_vogais(copia2));
+
+  //palindromo
+  printf("0- Se nao for palindromo\n 1- Se eh palindromo\n");
+  printf("Verificacao da primeira string:%d\n", meh_palindromo(copia1));
+  printf("Verificacao da segunda string:%d\n", meh_palindromo(copia2));
+
+  //Remover espaco da string teste:
+  mremove_espacos(copia1);
+  printf("A primeira string sem espacos : %s\n",copia1);
+
+  mremove_espacos(copia2);
+  printf("A segunda string sem espacos:%s\n",copia2);
+
+
+  return 0;
+}
+
+void mremove_espacos(char s[])
+{
+    int i, j = 0;
+
+    for(i = 0; s[i] != '\0'; i++)
+    {
+        if(s[i] != ' ')
+        {
+            s[j] = s[i];
+            j++;
+        }
+    }
+
+    s[j] = '\0';
 }
--- modularização/lista2/ex2/mstring.c
+++ modularização/lista2/ex2/mstring.c
@@ -1,4 +1,5 @@
 #include "mstring.h"
+#include <stdio.h>
 
 int mstrlen(char s[])
 {
@@ -19,5 +20,116 @@ int mstrlen(char s[])
         i++;
     }
     
-    return contador;
+    return contador - 1;
+}
+
+void mstrcpy(char dest[], char orig[])
+{
+    int i = 0;
+    while(orig[i] != '\0')
+        {
+            dest[i] = orig[i];
+            i++;
+        }
+        dest[i] = '\0';
+
+    printf("Copia: %s\n",orig);
+
+}
+
+void mstrcat(char dest[], char orig[])
+{
+    int i = 0;
+    int j = 0;
+
+    while (dest[i] != '\0') {
+        i++;
+    }
+
+    while (orig[j] != '\0') {
+        dest[i] = orig[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+}
+
+int mstrcmp(char a[], char b[])
+{
+    int i = 0;
+
+    while (a[i] != '\0' && b[i] != '\0')
+    {
+        if (a[i] < b[i])
+            return -1;
+
+        if (a[i] > b[i])
+            return 1;
+
+        i++;
+    }
+
+    if (a[i] == '\0' && b[i] == '\0')
+        return 0;
+
+    if (a[i] == '\0')
+        return -1;
+
+    return 1;
+}
+
+void mstrupper(char s[])
+{
+    int i;
+    for(i = 0 ; s[i] != '\0'; i++)
+    {
+        if(s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] = s[i] - 32;
+        }
+    }
+}
+
+void mstrlower(char s[])
+{
+    int i;
+
+    for(i = 0; s[i] != '\0'; i++)
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z')
+        {
+            s[i] += 32;
+        }
+    }
+}
+
+int mcount_vogais(char s[])
+{
+    int i;
+    int vogal = 0;
+    for(int i = 0; s[i] !='\0'; i++)
+    {
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'o' || s[i] == 'i' || s[i] == 'u')
+        {
+            vogal++;
+        }
+    }
+    return vogal;
+}
+
+int meh_palindromo(char s[])
+{
+    int i;
+    int tam = mstrlen(s);
+
+    for(i = 0; i < tam / 2; i++)
+    {
+        if(s[i] != s[tam - 1 - i])
+        {
+            return 0; 
+        }
+    }
+
+    return 1; 
 }
